90f406fe71e0bd18b67be06bca44e98bbdbda906
kewren <kwren@syr.edu>
2019-05-04 15:11:21 -0700

    Logging class addition. Does basic logging! Next up will be to integrate it into the TestHarness, maybe add some color. Also prints my time in 24 hour and off by an hour haha!

--- LocalTestHarness/main.cpp
+++ LocalTestHarness/main.cpp
@@ -3,6 +3,7 @@
 #include <functional>
 #include "DivideTest.h"
 #include "TestHarness.h"
+#include "Logger.h"
 
 
 //total number of testcase
@@ -11,6 +12,13 @@ const int TESTNUM = 5;
 int main(void) {
 	TestHarness harness(2);
 	DivideTest myTest(0);
+	Logger log(debug);
+	log.Debug("This is an Debug level statement.");
+	log.Info("This is an info level statement.");
+	log.Warning("This is an warning level statement.");
+	log.Error("This is an error level statement.");
+	log.Critical("This is an critical level statement.");
+
 	bool result = false;
 	for (int i = 0; i < TESTNUM; i++) {
 		myTest.setValue(i);
